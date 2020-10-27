d522d6ccaad49aa0a11abd6a5b75a59969c99271
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2020-10-28 00:22:42 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function handleSound(){
+  alert("hello")
+}
+
+document.querySelector("#SoundExtremism").addEventListener("click", handleSound);
+document.querySelector("#mixes").addEventListener("click", handleMixes);
+document.querySelector("#liveshows").addEventListener("click", handleLiveShows);
