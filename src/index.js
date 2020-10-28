36a42703d7de4f58469f20cd181dd4cb0ab93d0d
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2020-10-28 10:46:53 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 function handleSound(){
-  alert("hello")
+   window.location = 'soundextremism.html';
+}
+
+function handleMixes(){
+   window.location = 'mixes.html';
+}
+
+function handleLiveShows(){
+   window.location = 'liveshows.html';
+}
+
+function handleHome(){
+     window.location = 'home.html';
+}
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
 }
 
 document.querySelector("#SoundExtremism").addEventListener("click", handleSound);
 document.querySelector("#mixes").addEventListener("click", handleMixes);
 document.querySelector("#liveshows").addEventListener("click", handleLiveShows);
+document.querySelector("#archive").addEventListener("click", handleHome);
